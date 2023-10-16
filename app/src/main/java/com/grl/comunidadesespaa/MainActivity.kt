c266512df289e0a096e81379bcd036c0ca9b9c04
gabri <gabrielrl2004@gmail.com>
2023-10-16 10:06:17 +0200

    Context menu

--- app/src/main/java/com/grl/comunidadesespaa/MainActivity.kt
+++ app/src/main/java/com/grl/comunidadesespaa/MainActivity.kt
@@ -6,7 +6,9 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import com.grl.comunidadesespaa.adapter.ComunidadAdapter
 import com.grl.comunidadesespaa.databinding.ActivityMainBinding
 
@@ -22,11 +24,51 @@ class MainActivity : AppCompatActivity() {
         initRecycleView()
     }
 
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        lateinit var comunidadAfectada: Comunidad
+        comunidadAfectada = ComunidadProvider.comunidadList[item.itemId]
+        when (item.itemId) {
+            0 -> {
+                val alert =
+                    AlertDialog.Builder(this).setTitle("Eliminar ${comunidadAfectada.name}")
+                        .setMessage(
+                            "¿Estas seguro de que quieres eliminar ${comunidadAfectada.name}?"
+                        )
+                        .setNeutralButton("Cerrar", null).setPositiveButton(
+                            "Aceptar"
+                        ) { _, _ ->
+                            display("Se ha eliminado ${comunidadAfectada.name}")
+                            ComunidadProvider.comunidadList.removeAt(item.groupId)
+                            binding.rvComunidad.adapter?.notifyItemRemoved(item.groupId)
+                            binding.rvComunidad.adapter?.notifyItemRangeChanged(
+                                item.groupId,
+                                ComunidadProvider.comunidadList.size
+                            )
+                            binding.rvComunidad.adapter =
+                                ComunidadAdapter(ComunidadProvider.comunidadList) { comunidad ->
+                                    onItemSelected(comunidad)
+                                }
+                        }.create()
+                alert.show()
+            }
+
+            else -> return super.onContextItemSelected(item)
+        }
+
+
+
+        return true
+    }
+
     private fun initRecycleView() {
         val manager = LinearLayoutManager(this)
         binding.rvComunidad.layoutManager = manager
         binding.rvComunidad.adapter =
             ComunidadAdapter(ComunidadProvider.llenar()) { onItemSelected(it) }
+    }
+
+    private fun display(message: String) {
+        Snackbar.make(binding.root, message, Snackbar.LENGTH_SHORT).show()
     }
 
     private fun onItemSelected(comunidad: Comunidad) {
@@ -50,7 +92,7 @@ class MainActivity : AppCompatActivity() {
             binding.rvComunidad.adapter =
                 ComunidadAdapter(ComunidadProvider.llenar()) { onItemSelected(it) }
             binding.rvComunidad.adapter?.notifyDataSetChanged()
-        } else if(id == R.id.limpiar){
+        } else if (id == R.id.limpiar) {
             ComunidadProvider.comunidadList.clear()
             binding.rvComunidad.adapter?.notifyDataSetChanged()
         }
--- app/src/main/java/com/grl/comunidadesespaa/adapter/ComunidadViewHolder.kt
+++ app/src/main/java/com/grl/comunidadesespaa/adapter/ComunidadViewHolder.kt
@@ -6,22 +6,28 @@ import androidx.recyclerview.widget.RecyclerView
 import com.grl.comunidadesespaa.Comunidad
 import com.grl.comunidadesespaa.databinding.ItemComunidadBinding
 
-class ComunidadViewHolder(view: View):RecyclerView.ViewHolder(view), View.OnCreateContextMenuListener {
+class ComunidadViewHolder(view: View) : RecyclerView.ViewHolder(view),
+    View.OnCreateContextMenuListener {
 
     private var binding = ItemComunidadBinding.bind(view)
-    private lateinit var comunidad:Comunidad
+    private lateinit var comunidad: Comunidad
 
-    fun render(comunidad:Comunidad, onClickListener: (Comunidad)->Unit){
-        this.comunidad=comunidad
-        binding.name.text=comunidad.name
+    fun render(comunidad: Comunidad, onClickListener: (Comunidad) -> Unit) {
+        this.comunidad = comunidad
+        binding.name.text = comunidad.name
         binding.img.setImageResource(comunidad.image)
-         itemView.setOnClickListener{
-             onClickListener(comunidad)
-         }
+        itemView.setOnClickListener {
+            onClickListener(comunidad)
+        }
         itemView.setOnCreateContextMenuListener(this)
     }
 
-    override fun onCreateContextMenu(menu:ContextMenu?, v:View?, menuInfo:ContextMenu.ContextMenuInfo?){
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
         menu!!.setHeaderTitle(comunidad.name)
+        menu.add(this.adapterPosition, 0, 0, "Eliminar")
     }
 }
